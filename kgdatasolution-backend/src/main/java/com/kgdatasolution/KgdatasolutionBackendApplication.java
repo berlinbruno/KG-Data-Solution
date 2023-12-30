6e7114fa63d04f375ac201ddc437163910ba82da
Berlin Bruno <132148418+berlinbruno@users.noreply.github.com>
2023-12-30 20:47:51 +0530

    Setup

---
+++ kgdatasolution-backend/src/main/java/com/kgdatasolution/KgdatasolutionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kgdatasolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgdatasolutionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgdatasolutionBackendApplication.class, args);
+	}
+
+}
---
+++ kgdatasolution-backend/src/test/java/com/kgdatasolution/KgdatasolutionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgdatasolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgdatasolutionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
